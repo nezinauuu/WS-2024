735347108c65bbd7636b571a192f2b1ffe114bab
nezinauuu <b00133471@mytudublin.ie>
2024-03-25 14:50:04 +0000

    more stuff
    honestly idk

--- sample_api.py
+++ sample_api.py
@@ -1,20 +1,40 @@
 from flask import Flask
 from flask_restful import Resource, Api
 import json
-
+from flask_restful import reqparse
+from pymongo import MongoClient
+from bson.json_util import dumps, loads
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 api = Api(app)
 
 class GetProducts(Resource):
     def get(self):
-        return {'id': '1221'}
-        
- 
-             
+        client = MongoClient("mongodb://root:example@localhost:27017/")
+        db = client.sales
+        collection = db.sales_data
+        results = dumps(collection.find())
+        return json.loads(results)
+
 api.add_resource(GetProducts, '/getProducts')
 
+class GetTitles(Resource):
+    def get(self):
+        return {'id': '1221'}
 
-            
+api.add_resource(GetTitles, '/getTitles')
+
+class insertProducts(Resource):
+    def get(self):
+        return {'id': '1221'}
+
+api.add_resource(insertProducts, '/insertProducts')
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+api.add_resource(HelloWorld, '/')
+
 if __name__ == '__main__':
     app.run(debug=True)
